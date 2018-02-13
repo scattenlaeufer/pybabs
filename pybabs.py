6fe848f25600ca8730608c791265f42e032de8dd
Björn Guth <bjoern.guth@rwth-aachen.de>
2018-02-13 23:42:49 +0100

    Enable first rudimentary platoon creation

---
+++ pybabs.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+class UnitTooBigException(Exception):
+    pass
+
+
+class UnitTooSmallException(Exception):
+    pass
+
+
+class Platoon:
+
+    class Infantry_Unit:
+        pass
+
+
+    class Infantry_Squad(Infantry_Unit):
+
+        def __init__(self, quality, miniatur_cost, size, min_size, max_size):
+            self.nco = True
+            self.quality = quality
+            if min_size <= size <= max_size:
+                self.size = size
+            elif size < min_size:
+                raise(UnitTooSmallException)
+            else:
+                raise(UnitTooBigException)
+            self.cost = size * miniatur_cost
+
+
+    class HQ(Infantry_Unit):
+
+        def __init__(self, quality, officer_cost, soldiers, soldier_cost):
+            self.officer = True
+            self.quality = quality
+            if soldiers < 0:
+                raise(UnitTooSmallException)
+            elif soldiers > 2:
+                raise(UnitTooBigException)
+            self.size = 1 + soldiers
+            self.cost = officer_cost + soldiers * soldier_cost
+
+
+    def __init__(self, name):
+        self.name = name
+        self.infantry_squads = {}
+        self.hq = {}
+
+
+    def __str__(self):
+        out = '= ' + self.name + ' ='
+        if len(self.hq):
+            out += '\nHQ: ' + str(self.hq.keys())
+        if len(self.infantry_squads):
+            out += '\nSquads: ' + str(self.infantry_squads.keys())
+        return out
+
+
+    def add_hq(self, name, quality, officer_cost, soldiers, soldier_cost):
+        self.hq[name] = self.HQ(quality, officer_cost, soldiers, soldier_cost)
+
+
+    def add_infantry_squad(self, name, quality, miniature_cost, size, min_size, max_size):
+        self.infantry_squads[name] = self.Infantry_Squad(quality, miniature_cost, size, min_size, max_size)
+
+
+platoon = Platoon('Platoon 1')
+platoon.add_hq('First Lieutenant', 'Veteran', 90, 2, 13)
+platoon.add_infantry_squad('1st Squad', 'Veteran', 13, 10, 5, 10)
+print(platoon)
